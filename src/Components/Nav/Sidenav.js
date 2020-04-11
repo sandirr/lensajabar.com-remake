7ad782c26d20d200f0f23162b11cbd84b9a7ea9a
andi-irsandi <andi.ramadani765@gmail.com>
2020-04-11 07:26:07 +0700

    sidenav

--- src/Components/Nav/Sidenav.js
+++ src/Components/Nav/Sidenav.js
@@ -16,26 +16,51 @@ class Sidenav extends React.Component {
             </div>
           </div>
         </li>
+
+        <li>
+          <div className="blue-text text-darken-2">
+            <form style={{ cursor: "auto" }}>
+              <div className="input-field">
+                <input
+                  placeholder="Search something..."
+                  id="search"
+                  type="search"
+                  style={{
+                    paddingLeft:50
+                  }}
+                />
+                <label className="label-icon" htmlFor="search">
+                  <i className="tiny material-icons blue-text text-darken-2">
+                    search
+                  </i>
+                </label>
+              </div>
+            </form>
+          </div>
+        </li>
+        
         <li>
           <Link to="#!">
-            <i className="material-icons">cloud</i>First Link With Icon
+            <i className="material-icons">home</i>BERANDA
           </Link>
         </li>
         <li>
-          <Link to="#!">Second Link</Link>
+          <Link t="#test3">NASIONAL</Link>
         </li>
         <li>
-          <div className="divider"></div>
+          <Link t="#test4">DAERAH</Link>
         </li>
         <li>
-          <Link to="/" className="subheader">
-            Subheader
-          </Link>
+          <Link t="#test0">POLHUKAM</Link>
         </li>
         <li>
-          <Link className="waves-effect" to="#!">
-            Third Link With Waves
-          </Link>
+          <Link t="#test2">LENSA EKBIS</Link>
+        </li>
+        <li>
+          <Link t="#test2">PERISTIWA</Link>
+        </li>
+        <li>
+          <Link t="#test2">HUMANIORA</Link>
         </li>
       </ul>
     );
